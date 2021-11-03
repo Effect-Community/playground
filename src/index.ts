24da596ef28784414ed87cc11025f527f39686ab
Johannes Schickling <schickling.j@gmail.com>
2021-11-03 09:38:54 +0000

    added effect-ts/node runMain

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import '@effect-ts/core/Tracing/Enable'
 import { Effect as T, pipe } from '@effect-ts/core'
+import { runMain } from '@effect-ts/node/Runtime'
 
 const program = T.gen(function* ($) {
   yield* $(T.succeedWith(() => console.log('hello world')))
 })
 
-const main = () => {
-  pipe(program, T.runPromise)  
-}
-
-main()
+runMain(program)

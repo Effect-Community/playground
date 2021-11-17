b3fd5edafe2efe067b0276cfb44dea8f41a28e00
Michael Arnaldi <arnaldimichael@gmail.com>
2021-11-17 20:25:14 +0000

    update example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,58 @@
-import '@effect-ts/core/Tracing/Enable'
-import * as T from '@effect-ts/core/Effect'
-import { runMain } from '@effect-ts/node/Runtime'
-import { pipe } from '@effect-ts/core/Function'
+import * as T from "@effect-ts/core/Effect"
+import * as L from "@effect-ts/core/Effect/Layer"
+import { pipe } from "@effect-ts/core/Function"
+import { runMain } from "@effect-ts/node/Runtime"
+import { service, tag } from "@effect-ts/core/Has"
+import { _A } from "@effect-ts/core/Utils"
 
-const program = T.gen(function* ($) {
-  yield* $(T.succeedWith(() => console.log('hello world')))
+// 
+// Service Constructor
+//
 
-  const result = yield* $(pipe(
-    T.succeed(1),
-    T.map(_ => _ * 2),
-    T.map(_ => _ * 3) ,
-  ))
+export const LoggerId = Symbol()
 
-  yield* $(T.succeedWith(() => console.log({ result })))
+export const makeLogger = T.succeedWith(() => {
+  return service(LoggerId, {
+    log: (s: string) => T.succeedWith(() => {
+      console.log(s)
+    })
+  })
 })
 
-runMain(program)
+// 
+// Service Constructor
+//
+
+export interface Logger extends _A<typeof makeLogger> { }
+export const Logger = tag<Logger>(LoggerId)
+
+// 
+// Layer to compose constructors (like LiveLogger["+++"](LiveDatabase)[">+>"](LiveUserRepo))
+//
+
+export const LiveLogger = L.fromEffect(Logger)(makeLogger)
+
+//
+// Access functions (here there is an annotation)
+//
+export const { log } = T.deriveLifted(Logger)(["log"], [], [])
+
+//
+// Any type of program, freely uses log without any explicit annotation, the type will be T.Effect<Has<Logger>, never, void>
+//
+
+export const program = pipe(
+  T.do,
+  T.bind("a", () => T.succeedWith(() => 1)),
+  T.bind("b", () => T.succeedWith(() => 2)),
+  T.bind("c", ({ a, b }) => T.succeedWith(() => a + b)),
+  T.chain((result) => log(`result: ${result}`))
+)
+
+pipe(
+  program,
+  // provides all the dependencies
+  T.provideSomeLayer(LiveLogger),
+  // runs the program
+  runMain
+)
